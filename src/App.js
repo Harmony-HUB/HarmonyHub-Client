4a230218145476853823a2a2062b730318272f73
taewoo124 <121784425+taewoo124@users.noreply.github.com>
2023-04-09 14:22:44 +0900

    feat: Modal 컴포넌트 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,60 @@
-import React from "react";
+import { useState } from "react";
+import styled from "styled-components";
+import Button from "./components/common/Button";
+import MusicEditor from "./components/Editor";
+import Modal from "./components/Modal";
+
+const Header = styled.header`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  width: 88%;
+  height: 15%;
+  padding: 1rem 2rem;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #f0f0f0;
+`;
+
+const Title = styled.h1`
+  font-size: 2.5rem;
+  color: #3bd6c6;
+  margin-bottom: 1rem;
+`;
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <div>
-      <p>하이</p>
-    </div>
+    <Container>
+      <Header>
+        <Button onClick={openModal}>Login</Button>
+      </Header>
+      <Title>Harmony HUB</Title>
+      <MusicEditor />
+      <Modal isOpen={isModalOpen} onClose={closeModal}>
+        {/* Add your modal content here */}
+        <h2>Login Form</h2>
+        {/* Include your login form components here */}
+      </Modal>
+    </Container>
   );
 }
 
