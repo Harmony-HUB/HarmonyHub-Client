f65c3c7dbfd2e70feb76c34cca8310e0623c4ae4
taewoo124 <121784425+taewoo124@users.noreply.github.com>
2023-08-20 02:03:51 +0900

    fix: netlify 배포 에러 수정

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,21 +1,18 @@
 import { Route, Routes, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import { useMediaQuery } from "react-responsive";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import MusicEditor from "./pages/MusicEditor/MusicEditor";
 import Login from "./pages/Login/Login";
 import AudioRecorder from "./pages/AudioRecorder/AudioRecorder";
 import { GuideDesktop, Title, Container } from "./styles";
 import getApiWithToken from "./pages/MusicList/api";
 import { setUserData } from "./feature/userDataSlice";
-import { RootState } from "./store";
 
 function MainApp() {
   const isDesktopOrLaptop = useMediaQuery({ query: "(min-width: 1274px)" });
   const dispatch = useDispatch();
   const navigate = useNavigate();
-
-  const { data, loading } = useSelector((state: RootState) => state.userData);
 
   useEffect(() => {
     const reloadPage = async () => {
@@ -24,17 +21,12 @@ function MainApp() {
         dispatch(setUserData(user));
       } catch (error) {
         console.error("Error fetching user data:", error);
+        navigate("/login");
       }
     };
 
     reloadPage();
   }, []);
-
-  useEffect(() => {
-    if (!data && !loading) {
-      navigate("/login");
-    }
-  }, [data]);
 
   if (!isDesktopOrLaptop) {
     return (
